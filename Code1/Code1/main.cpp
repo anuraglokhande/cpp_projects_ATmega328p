e3c4761d8f1f6c45d211c3232bc2a029ea70255d
kit <45459801+anuraglokhande@users.noreply.github.com>
2019-01-03 23:17:01 +0530

    cpp_projects_ATmega328p This folder is for only making cpp projects of ATmega328p and learning purpose of cpp for ATmega328p

---
+++ Code1/Code1/main.cpp
@@ -0,0 +1,57 @@
+/*
+ * Code1.cpp
+ *
+ * Created: 03/01/2019 7:46:31 PM
+ * Author : Anurag
+ * code function: this code is to check only "how to work with class in embedded system and
+ * is it work or not"
+ * Aim of project: Blinking of On board LED of Arduino nano
+ */ 
+
+#include <avr/io.h>
+
+#define F_CPU 8000000UL	//need to put before #include <util/delay.h>
+						//otherwise it will throw #warning "F_CPU not defined for <util/delay.h>"
+						//and in program, resolution of 1sec will be wrong 
+#include <util/delay.h>
+
+
+#define DELAY_MS 1000
+
+class Box {
+	public:
+	/* setting port pin for led blinking */
+	
+	void setFunction_port_b(void)
+	{
+		/* This will ON the on board LED ON of Arduino nano  */
+			PORTB = 0xff;	//make PORT B as OUTPUT
+			DDRB = 0xff;	//make PORT B all pins HIGH
+	}
+	
+	void clearFunction_port_b(void)
+	{
+		/* This will OFF the on board LED ON of Arduino nano  */
+		PORTB = 0xff;	//make PORT B as OUTPUT
+		DDRB = 0x00;	//make PORT B all pins LOW
+	}
+	
+};
+
+
+int main(void)
+{
+    /* Replace with your application code */
+	Box Box1;        // Declare Box1 of type Box
+	
+
+	
+    while (1) 
+    {
+			Box1.setFunction_port_b();
+			_delay_ms(DELAY_MS);
+			Box1.clearFunction_port_b();
+			_delay_ms(DELAY_MS);
+    }
+}
+
